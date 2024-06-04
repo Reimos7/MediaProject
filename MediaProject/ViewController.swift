7582afb66e03bf7732d2ceb3e76507b7fb63bed7
Reimos7 <tjrvud9@gmail.com>
2024-06-05 00:17:34 +0900

    Initial Commit

---
+++ MediaProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediaProject
+//
+//  Created by 홍석평 on 6/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
